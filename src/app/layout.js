b00b9b498bc8e5941f6bb1711eaf5a9712f45e4f
imranrizvi648 <de.imran@brbgroup.pk>
2026-07-18 17:08:54 +0600

    add widget

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import FluidCursor from "./components/FluidCursor";
@@ -103,6 +103,14 @@ export default function RootLayout({ children }) {
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
+  <Script 
+  src="http://203.130.9.165:18086/widget/wid_c019a568ccd14a05.js"
+
+  strategy="lazyOnload"
+/>
+
+
+
       </body>
     </html>
   );
